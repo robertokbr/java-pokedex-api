5d89bffd81a0740f14bc3ad9ebd0035e650990d6
robertokbr <robertojuniordev@gmail.com>
2021-05-29 16:07:46 -0300

    Add init method

--- src/main/java/com/robertojr/pokedex/PokedexApplication.java
+++ src/main/java/com/robertojr/pokedex/PokedexApplication.java
@@ -1,7 +1,15 @@
 package com.robertojr.pokedex;
 
+import com.robertojr.pokedex.models.Pokemon;
+import com.robertojr.pokedex.repositories.PokedexRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import reactor.core.publisher.Flux;
+
 
 @SpringBootApplication
 public class PokedexApplication {
@@ -10,4 +18,17 @@ public class PokedexApplication {
 		SpringApplication.run(PokedexApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(ReactiveMongoOperations operations, PokedexRepository repository) {
+		return args -> {
+			Flux<Pokemon> pokedexFlux = Flux.just(
+				new Pokemon(null, "Bulbassauro", "Semente", "OverGrow", 6.09),
+				new Pokemon(null, "Charizard", "Fogo", "Blaze", 90.05),
+				new Pokemon(null, "Caterpie", "Minhoca", "Poeira do Escudo", 2.09),
+				new Pokemon(null, "Blastoise", "Marisco", "Torrente", 6.09)
+			).flatMap(repository::save);
+		
+			pokedexFlux.thenMany(repository.findAll()).subscribe(System.out::println);
+		};
+	}
 }
